6d610e72f407f50dafd01ce23d07e045d9d7b7fa
viktor kodolov <vkodolov@tamashi.games>
2019-06-30 22:47:12 +0700

    Create TouchController

---
+++ Assets/ZigZag/Scripts/Controlls/TouchController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using Zenject;
+
+namespace Game
+{
+  public class TouchController : MonoBehaviour, IPointerDownHandler
+  {
+    [Inject] private SignalBus _signal_bus;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+      _signal_bus.Fire<Touch>();
+    }
+  }
+}
