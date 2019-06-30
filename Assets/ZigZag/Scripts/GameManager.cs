428d0db9694e380ee63222c9950379113927621b
viktor kodolov <vkodolov@tamashi.games>
2019-06-30 18:05:50 +0700

    Add GameManager

---
+++ Assets/ZigZag/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Zenject;
+
+namespace Game {
+  public class GameManager : IInitializable
+  {
+    [Inject] private Settings _settings;
+    [Inject] private SignalBus _signal_bus;
+
+    public Difficulty CurrentDifficulty
+    {
+      // TODO
+      get
+      {
+        return _settings.Difficulties[2];
+      }
+    }
+
+    public void Initialize()
+    {
+      _signal_bus.Fire<GameStartSignal>();
+      _signal_bus.Subscribe<GameEndSignal>(_on_game_end);
+    }
+
+    private void _on_game_end()
+    {
+
+    }
+
+    [Serializable]
+    public class Difficulty
+    {
+      public enum Difficulties
+      {
+        Easy,
+        Medium,
+        Hard
+      }
+
+      public Difficulties difficulty;
+      public int TileWidth;
+    }
+
+    [Serializable]
+    public class Settings
+    {
+      public Difficulty[] Difficulties;
+    }
+  }
+}
