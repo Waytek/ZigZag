4099188e64fa7e250bd744ad73c47962f6e5d01d
viktor kodolov <vkodolov@tamashi.games>
2019-06-30 22:59:41 +0700

    GameInstaller - bind playerFactory, fix bind GameManager and tile spawned

--- Assets/ZigZag/Scripts/Installers/GameInstaller.cs
+++ Assets/ZigZag/Scripts/Installers/GameInstaller.cs
@@ -8,14 +8,17 @@ namespace Game
   {
     [Inject] private TileHelper.Settings _tile_settings;
     [Inject] private CrystalHelper.Settings _crystal_settings;
+    [Inject] private PlayerHelper.Settings _player_settings;
 
     public override void InstallBindings()
     {
       GameSignalsInstaller.Install(Container);
 
-      Container.BindInterfacesAndSelfTo<TileSpawnManager>().AsSingle();
-      Container.BindInterfacesAndSelfTo<GameManager>().AsSingle();
       Container.BindInterfacesAndSelfTo<CrystalSpawnManager>().AsSingle();
+      Container.BindInterfacesAndSelfTo<GameManager>().AsSingle();
+      Container.BindInterfacesAndSelfTo<TileSpawnManager>().AsSingle();
+      
+      
 
       Container.BindFactory<Vector3, int, TileHelper.Directions, TileHelper, TileHelper.Factory>()
           .FromPoolableMemoryPool<Vector3, int, TileHelper.Directions, TileHelper, TileHelperPool>(TileBinder => TileBinder
@@ -27,6 +30,9 @@ namespace Game
           .FromPoolableMemoryPool<TileHelper, CrystalHelper, CrystalHelperPool>(CrystalBinder => CrystalBinder
               .WithInitialSize(10)
               .FromComponentInNewPrefab(_crystal_settings.Prefab));
+
+      Container.BindFactory<PlayerHelper, PlayerHelper.Factory>()
+        .FromComponentInNewPrefab(_player_settings.Prefab);
     }
 
     class TileHelperPool : MonoPoolableMemoryPool<Vector3, int, TileHelper.Directions, IMemoryPool, TileHelper> { }
