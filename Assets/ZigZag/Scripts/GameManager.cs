66758df7a4c14bb5b604774ccc5121716213808b
viktor kodolov <vkodolov@tamashi.games>
2019-06-30 22:58:53 +0700

    GameManager - add spawn player, add wait touh after spawn before game started, add difficulties curve changed of game time,

--- Assets/ZigZag/Scripts/GameManager.cs
+++ Assets/ZigZag/Scripts/GameManager.cs
@@ -1,33 +1,54 @@
 ﻿using System;
+using UnityEngine;
 using Zenject;
 
-namespace Game {
-  public class GameManager : IInitializable
+namespace Game
+{
+  public class GameManager : IInitializable, ITickable
   {
     [Inject] private Settings _settings;
     [Inject] private SignalBus _signal_bus;
+    [Inject] private PlayerHelper.Factory _player_factory;
 
     public Difficulty CurrentDifficulty
     {
-      // TODO
       get
       {
-        return _settings.Difficulties[2];
+        int difficulties = Mathf.Clamp((int)_settings._Difficulties_Curve.Evaluate(_game_time), 0, _settings.Difficulties.Length - 1);
+        return _settings.Difficulties[difficulties];
       }
     }
 
     public int CurrentScore{ get; private set;}
 
+    private float _game_time = 0f;
+
     public void Initialize()
     {
-      _signal_bus.Fire<GameStartSignal>();
-      _signal_bus.Subscribe<GameEndSignal>(_on_game_end);
+      _signal_bus.Subscribe<GameOverSignal>(_on_game_end);
       _signal_bus.Subscribe<CrystalPickUpSignal>(() => CurrentScore++);
+      _player_factory.Create();
+      _signal_bus.Subscribe<Touch>(_on_touch);
+    }
+
+    private void _on_touch()
+    {
+      _signal_bus.Unsubscribe<Touch>(_on_touch);
+      _signal_bus.Fire<GameStartSignal>();
+      _game_time = 0f;
     }
 
     private void _on_game_end()
     {
-      
+      _player_factory.Create();
+      _signal_bus.Subscribe<Touch>(_on_touch);
+      CurrentScore = 0;
+      _game_time = 0f;
+    }
+
+    public void Tick()
+    {
+      _game_time += Time.deltaTime;
     }
 
     [Serializable]
@@ -48,6 +69,7 @@ namespace Game {
     public class Settings
     {
       public Difficulty[] Difficulties;
+      public AnimationCurve _Difficulties_Curve;
     }
   }
 }
