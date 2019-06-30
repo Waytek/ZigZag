da7fe4f89dd9806b79cd997f1ac63cd92c134f64
viktor kodolov <vkodolov@tamashi.games>
2019-06-30 20:40:13 +0700

    GameInstaller - bind CrystalSpawnManager singletone, bind memory pool CrystalHelper

--- Assets/ZigZag/Scripts/Installers/GameInstaller.cs
+++ Assets/ZigZag/Scripts/Installers/GameInstaller.cs
@@ -5,8 +5,9 @@ namespace Game
 {
   // Main installer for our game
   public class GameInstaller : MonoInstaller
-    {
-        [Inject] private TileHelper.Settings _tile_settings;
+  {
+    [Inject] private TileHelper.Settings _tile_settings;
+    [Inject] private CrystalHelper.Settings _crystal_settings;
 
     public override void InstallBindings()
     {
@@ -14,8 +15,7 @@ namespace Game
 
       Container.BindInterfacesAndSelfTo<TileSpawnManager>().AsSingle();
       Container.BindInterfacesAndSelfTo<GameManager>().AsSingle();
-      
-
+      Container.BindInterfacesAndSelfTo<CrystalSpawnManager>().AsSingle();
 
       Container.BindFactory<Vector3, int, TileHelper.Directions, TileHelper, TileHelper.Factory>()
           .FromPoolableMemoryPool<Vector3, int, TileHelper.Directions, TileHelper, TileHelperPool>(TileBinder => TileBinder
@@ -23,11 +23,13 @@ namespace Game
               .FromComponentInNewPrefab(_tile_settings.Prefab)
               .UnderTransformGroup("MapTiles"));
 
-      
+      Container.BindFactory<TileHelper, CrystalHelper, CrystalHelper.Factory>()
+          .FromPoolableMemoryPool<TileHelper, CrystalHelper, CrystalHelperPool>(CrystalBinder => CrystalBinder
+              .WithInitialSize(10)
+              .FromComponentInNewPrefab(_crystal_settings.Prefab));
     }
 
-    class TileHelperPool : MonoPoolableMemoryPool<Vector3, int, TileHelper.Directions, IMemoryPool, TileHelper>
-    {
-    }
+    class TileHelperPool : MonoPoolableMemoryPool<Vector3, int, TileHelper.Directions, IMemoryPool, TileHelper> { }
+    class CrystalHelperPool : MonoPoolableMemoryPool<TileHelper, IMemoryPool, CrystalHelper> { }
   }
 }
