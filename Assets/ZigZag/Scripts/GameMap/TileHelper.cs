3e03a69697823542513e727c52383ca18d772dca
viktor kodolov <vkodolov@tamashi.games>
2019-06-30 20:36:57 +0700

    TileHelper - add parameter to Fire TileSpawnSignal, add exeption on default switch branch

--- Assets/ZigZag/Scripts/GameMap/TileHelper.cs
+++ Assets/ZigZag/Scripts/GameMap/TileHelper.cs
@@ -4,76 +4,86 @@ using System.Collections.Generic;
 using UnityEngine;
 using Zenject;
 
-public class TileHelper : MonoBehaviour, IPoolable<Vector3, int, TileHelper.Directions, IMemoryPool>
+namespace Game
 {
-  public enum Directions
+  public class TileHelper : MonoBehaviour, IPoolable<Vector3, int, TileHelper.Directions, IMemoryPool>
   {
-    Forward,
-    Right
-  }
-
-  [Inject] private Settings _settings;
-
-  [SerializeField]
-  private Transform spawnPoint;
-
-  public int Widt { get; private set; }
-  public Directions Direction { get; private set; }
-
-  private IMemoryPool _pool;
-
-  public void Destroy()
-  {
-    Timing.RunCoroutine(_despawn_coroutine());
-  }
-
-  public void OnSpawned(Vector3 position, int width, Directions direction, IMemoryPool pool)
-  {
-    transform.position = position;
-    _pool = pool;
-    Widt = width;
-    Direction = direction;
-    transform.localScale = new Vector3(width, _settings.Height, _settings.Height);
-    switch (direction)
+    public enum Directions
     {
-      case Directions.Forward:
-        transform.rotation = Quaternion.Euler(_settings.ForwardRotation);
-        break;
-      case Directions.Right:
-        transform.rotation = Quaternion.Euler(_settings.RightRotation);
-        break;
-      default:
-        break;
+      Forward,
+      Right
     }
-  }
 
-  public void OnDespawned()
-  {
-    _pool = null;
-  }
+    [Inject] private Settings _settings;
 
-  private IEnumerator<float> _despawn_coroutine()
-  {
-    float time = 0;
-    while(time < _settings.DestroyTime)
+    [SerializeField]
+    private Transform spawnPoint;
+
+    public int Widt { get; private set; }
+    public Directions Direction { get; private set; }
+
+    public CrystalHelper Crystal;
+
+    private IMemoryPool _pool;
+
+    public void Destroy()
     {
-      transform.position -= transform.up * _settings.DestroyAnimationSpeed * Time.deltaTime;
-      time += Time.deltaTime;
-      yield return Timing.WaitForOneFrame;
+      Timing.RunCoroutine(_despawn_coroutine());
+      if (Crystal)
+      {
+        Crystal.Destroy();
+      }
     }
-    _pool.Despawn(this);
-  }
 
-  [Serializable]
-  public class Settings
-  {
-    public TileHelper Prefab;
-    public float Height = 1f;
-    public Vector3 ForwardRotation = new Vector3(0,0,0);
-    public Vector3 RightRotation = new Vector3(90,0,0);
-    public float DestroyTime = 3f;
-    public float DestroyAnimationSpeed = 5f;
-  }
+    public void OnSpawned(Vector3 position, int width, Directions direction, IMemoryPool pool)
+    {
+      transform.position = position;
+      _pool = pool;
+      Widt = width;
+      Direction = direction;
+      transform.localScale = new Vector3(width, _settings.Height, _settings.Height);
+      switch (direction)
+      {
+        case Directions.Forward:
+          transform.rotation = Quaternion.Euler(_settings.ForwardRotation);
+          break;
+        case Directions.Right:
+          transform.rotation = Quaternion.Euler(_settings.RightRotation);
+          break;
+        default:
+          throw new ArgumentException($"On spawned TileHelper with direction {direction} used default");
+      }
+    }
 
-  public class Factory : PlaceholderFactory<Vector3 ,int, Directions, TileHelper>{}
+    public void OnDespawned()
+    {
+      _pool = null;
+      Crystal = null;
+    }
+
+    private IEnumerator<float> _despawn_coroutine()
+    {
+      float time = 0;
+      while (time < _settings.DestroyTime)
+      {
+        transform.position -= transform.up * _settings.DestroyAnimationSpeed * Time.deltaTime;
+        time += Time.deltaTime;
+        yield return Timing.WaitForOneFrame;
+      }
+      _pool.Despawn(this);
+    }
+
+    [Serializable]
+    public class Settings
+    {
+      public TileHelper Prefab;
+      public float Height = 1f;
+      public Vector3 ForwardRotation = new Vector3(0, 0, 0);
+      public Vector3 RightRotation = new Vector3(90, 0, 0);
+      public float DestroyTime = 3f;
+      public float DestroyAnimationSpeed = 5f;
+    }
+
+    public class Factory : PlaceholderFactory<Vector3, int, Directions, TileHelper> { }
+  }
 }
