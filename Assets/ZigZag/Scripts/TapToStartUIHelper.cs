e2aadc4f16a9b3456af819fc1f4b5f40548360c3
viktor kodolov <vkodolov@tamashi.games>
2019-06-30 23:16:33 +0700

    Add TapToStartUIHelper

---
+++ Assets/ZigZag/Scripts/TapToStartUIHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace Game {
+  public class TapToStartUIHelper : MonoBehaviour
+  {
+    [Inject] private SignalBus _signal_bus;
+
+    private void Start()
+    {
+      _signal_bus.Subscribe<GameStartSignal>(_on_game_start);
+      _signal_bus.Subscribe<GameOverSignal>(_on_game_over);
+    }
+
+    private void _on_game_start()
+    {
+      gameObject.SetActive(false);
+    }
+
+    private void _on_game_over()
+    {
+      gameObject.SetActive(true);
+    }
+  }
+}
