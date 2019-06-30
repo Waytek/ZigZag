02208a09e6eb8cbeed7037fa72137d95bcc7952d
viktor kodolov <vkodolov@tamashi.games>
2019-06-30 18:06:09 +0700

    Create GameEvents

---
+++ Assets/ZigZag/Scripts/GameEvents.cs
@@ -0,0 +1,12 @@
+﻿namespace Game
+{
+
+  public struct GameStartSignal { }
+
+  public struct GameEndSignal { }
+
+  public struct TileSpawnSignal { }
+
+  public struct CrystalPickUpSignal { }
+
+}
