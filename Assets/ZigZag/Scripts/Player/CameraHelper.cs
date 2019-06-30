ad190e2319f50044813401801f6aefeb3967f98a
viktor kodolov <vkodolov@tamashi.games>
2019-06-30 22:53:08 +0700

    Create CameraHelper

---
+++ Assets/ZigZag/Scripts/Player/CameraHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game
+{
+  public class CameraHelper : MonoBehaviour
+  {
+    [SerializeField]
+    private float speed;
+
+    private Transform _target;
+
+    private void Start()
+    {
+      _target = transform.parent;
+      if (!_target)
+      {
+        throw new ArgumentException($"Cant get target for CameraHelper {gameObject.name}");
+      }
+      transform.parent = null;
+    }
+
+    private void Update()
+    {
+      if (_target)
+      {
+        transform.position = Vector3.MoveTowards(transform.position, _target.position, speed * Time.deltaTime);
+      }
+    }
+  }
+}
